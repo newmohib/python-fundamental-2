1b69d237327ba51d73291eb34a939a40d410288f
mohibbs <mohib@bs-23.net>
2023-06-19 10:56:01 +0600

    added: decorators

--- Simple_Decorators.py
+++ Simple_Decorators.py
@@ -1,14 +1,15 @@
+import functools
+
+# The @ syntax decorator
+
 
 # Simple Decorators in python
 
 user = {"username": "jose", "access_level": "guest"}
 
 
-def get_admin_password():
-    return "12345"
-
-
 def make_secure(func):
+    @functools.wraps(func)
     def secure_function():
         if user["access_level"] == "admin":
             return func()
@@ -17,7 +18,67 @@ def make_secure(func):
     return secure_function
 
 
-get_admin_password = make_secure(get_admin_password)
+@make_secure
+def get_admin_password():
+    return "12345"
 
-print(get_admin_password)
 
+# get_admin_password = make_secure(get_admin_password)
+
+# print(get_admin_password)
+print(get_admin_password())
+
+
+# Decorator functions with parametars
+
+
+def make_secure(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if user["access_level"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            return f"No admin permissions for {user['username']}"
+    return secure_function
+
+
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    else:
+        return "super_secure_password"
+
+
+print(get_password("billing"))
+
+
+# Decorator with parametars
+
+user = {"username": "jose", "access_level": "admin"}
+
+
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}"
+        return secure_function
+    return decorator
+
+
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 1234"
+
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+print(get_admin_password())
+print(get_dashboard_password())
---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+# mutability python
+
+a = []
+b = a
